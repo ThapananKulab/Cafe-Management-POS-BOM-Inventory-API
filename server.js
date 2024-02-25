da07dbd3c2ee8d3d22fd6c5eca2dcc6375778335
ThapananKulab <nicekrubma123@gmail.com>
2024-02-25 16:37:53 +0700

    update

--- server.js
+++ server.js
@@ -19,20 +19,41 @@ const cookieParser = require('cookie-parser')
 
 app.use(cookieParser())
 
-app.use(expressSession({
-  cookie: { maxAge: 86400000 },
-  store: new MemoryStore({
-    checkPeriod: 86400000, // prune expired entries every 24h
-  }),
-  resave: false,
-  saveUninitialized: true, // Add this line
-  secret: 'Fullstack',
-}));
+app.use(
+  expressSession({
+    cookie: { maxAge: 86400000 },
+    store: new MemoryStore({
+      checkPeriod: 86400000, // prune expired entries every 24h
+    }),
+    resave: false,
+    saveUninitialized: true, // Add this line
+    secret: 'Fullstack',
+  })
+)
 
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('Server is running')
+// app.get('/', (req, res) => {
+//   res.send('Server is running')
+// })
+app.get('/', function (req, res, next) {
+  Task.find()
+    .then((tasks) => {
+      const currentTasks = tasks.filter((task) => !task.completed)
+      const completedTasks = tasks.filter((task) => task.completed === true)
+
+      console.log(
+        `Total tasks: ${tasks.length}   Current tasks: ${currentTasks.length}    Completed tasks:  ${completedTasks.length}`
+      )
+      res.render('index', {
+        currentTasks: currentTasks,
+        completedTasks: completedTasks,
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+      res.send('Sorry! Something went wrong.')
+    })
 })
 
 // app.use(express.static(dist));

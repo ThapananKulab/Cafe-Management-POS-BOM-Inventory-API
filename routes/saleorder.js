a8bc390768651034e06530c2bdfc31f947e4087a
ThapananKulab <nicekrubma123@gmail.com>
2024-03-30 18:40:05 +0700

    update

--- routes/saleorder.js
+++ routes/saleorder.js
@@ -33,6 +33,30 @@ router.get('/saleOrders', async (req, res) => {
   }
 })
 
+router.get('/saleOrders/currentdate', async (req, res) => {
+  try {
+    // Set the start and end of the current day
+    const startOfToday = new Date()
+    startOfToday.setHours(0, 0, 0, 0) // Sets the time to the start of the day
+
+    const endOfToday = new Date()
+    endOfToday.setHours(23, 59, 59, 999) // Sets the time to the end of the day
+
+    // Find orders where the date is within the current day
+    const saleOrders = await SaleOrder.find({
+      date: {
+        // Use the 'date' field
+        $gte: startOfToday, // Greater than or equal to the start of today
+        $lte: endOfToday, // Less than or equal to the end of today
+      },
+    })
+
+    res.json(saleOrders)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+})
+
 router.post('/orders', async (req, res) => {
   try {
     const { orderNumber, user, items, total, status, paymentMethod, notes } =

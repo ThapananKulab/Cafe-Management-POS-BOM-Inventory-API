45baaa1e38c74da5e638c909f6f3c6d7a3a6375e
ThapananKulab <nicekrubma123@gmail.com>
2024-04-03 01:26:36 +0700

    update

--- models/InventoryItem.js
+++ models/InventoryItem.js
@@ -1,24 +1,29 @@
 // อย่าลืมนำเข้า mongoose และกำหนด schema ของคุณ
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const inventoryItemSchema = new mongoose.Schema({
   // order: { type: Number }, // เพิ่มฟิลด์นี้เพื่อเก็บลำดับ
   name: { type: String, required: true },
+  type: {
+    type: String,
+    required: true,
+    enum: ["กรัม", "มิลลิลิตร", "ชิ้น", "ซอง", "ทั่วไป"],
+  },
   unit: {
     type: String,
     required: true,
-    enum: ['กรัม', 'มิลลิลิตร', 'ชิ้น', 'ซอง', 'ทั่วไป'],
+    enum: ["กรัม", "มิลลิลิตร", "ชิ้น", "ซอง", "ทั่วไป"],
   },
   realquantity: { type: Number, required: true },
   quantityInStock: { type: Number, required: true },
   useInStock: { type: Number, required: true, default: 0 },
   unitPrice: { type: Number, required: true },
-})
+});
 
 // เพิ่ม method สำหรับการปรับปรุงสต็อก
 inventoryItemSchema.methods.adjustStock = async function (amount) {
-  this.quantityInStock += amount
-  await this.save()
-}
+  this.quantityInStock += amount;
+  await this.save();
+};
 
-module.exports = mongoose.model('InventoryItem', inventoryItemSchema)
+module.exports = mongoose.model("InventoryItem", inventoryItemSchema);
--- routes/menus.js
+++ routes/menus.js
@@ -1,27 +1,27 @@
-const express = require('express')
-const router = express.Router()
-const Menu = require('../models/Menu.js')
-const cloudinary = require('../utils/cloudinary.js')
-const multer = require('multer')
+const express = require("express");
+const router = express.Router();
+const Menu = require("../models/Menu.js");
+const cloudinary = require("../utils/cloudinary.js");
+const multer = require("multer");
 
-const storage = multer.diskStorage({})
-const parser = multer({ storage: storage })
+const storage = multer.diskStorage({});
+const parser = multer({ storage: storage });
 
-router.post('/addMenu', parser.single('image'), async (req, res) => {
-  const { name, description, price, sweetLevel, type, recipe } = req.body
+router.post("/addMenu", parser.single("image"), async (req, res) => {
+  const { name, description, price, sweetLevel, type, recipe } = req.body;
 
   if (!req.file) {
     return res
       .status(400)
-      .json({ success: false, message: 'No image file provided.' })
+      .json({ success: false, message: "No image file provided." });
   }
 
-  const imageUrl = req.file.path
+  const imageUrl = req.file.path;
 
   try {
     const result = await cloudinary.uploader.upload(imageUrl, {
-      folder: 'menus',
-    })
+      folder: "menus",
+    });
 
     const menuItem = new Menu({
       name,
@@ -31,37 +31,37 @@ router.post('/addMenu', parser.single('image'), async (req, res) => {
       type,
       recipe,
       image: result.secure_url,
-    })
-    const savedItem = await menuItem.save()
+    });
+    const savedItem = await menuItem.save();
     res.status(201).json({
       success: true,
       menuItem: savedItem,
-    })
+    });
   } catch (error) {
-    console.error(error)
-    res.status(400).json({ success: false, message: 'Error adding menu item' })
+    console.error(error);
+    res.status(400).json({ success: false, message: "Error adding menu item" });
   }
-})
+});
 
-router.get('/allMenus', async (req, res) => {
+router.get("/allMenus", async (req, res) => {
   try {
-    const menuItems = await Menu.find().populate('recipe')
-    res.status(200).json(menuItems)
+    const menuItems = await Menu.find().populate("recipe");
+    res.status(200).json(menuItems);
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message });
   }
-})
+});
 
 // DELETE a menu item by ID
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
-    const { id } = req.params
-    const deletedItem = await Menu.findByIdAndDelete(id)
+    const { id } = req.params;
+    const deletedItem = await Menu.findByIdAndDelete(id);
 
     if (!deletedItem) {
       return res
         .status(404)
-        .json({ success: false, message: 'Menu item not found' })
+        .json({ success: false, message: "Menu item not found" });
     }
 
     // Optionally, you can also delete the associated image from Cloudinary here
@@ -70,43 +70,43 @@ router.delete('/:id', async (req, res) => {
 
     res.json({
       success: true,
-      message: 'Menu item deleted successfully',
+      message: "Menu item deleted successfully",
       deletedItem: deletedItem,
-    })
+    });
   } catch (error) {
-    console.error(error)
+    console.error(error);
     res
       .status(500)
-      .json({ success: false, message: 'Error deleting menu item' })
+      .json({ success: false, message: "Error deleting menu item" });
   }
-})
+});
 
-router.get('/checkName', async (req, res) => {
+router.get("/checkName", async (req, res) => {
   try {
-    const { name } = req.query // รับชื่อเมนูจาก query parameter
-    const menuItem = await Menu.findOne({ name }) // ค้นหาเมนูที่มีชื่อตรงกับที่ระบุ
+    const { name } = req.query; // รับชื่อเมนูจาก query parameter
+    const menuItem = await Menu.findOne({ name }); // ค้นหาเมนูที่มีชื่อตรงกับที่ระบุ
 
     if (menuItem) {
       // ถ้าพบเมนูนี้ในฐานข้อมูล
       res.json({
         exists: true,
-        message: 'A menu item with this name already exists.',
-      })
+        message: "A menu item with this name already exists.",
+      });
     } else {
       // ถ้าไม่พบ
-      res.json({ exists: false, message: 'Name is available.' })
+      res.json({ exists: false, message: "Name is available." });
     }
   } catch (error) {
-    console.error(error)
+    console.error(error);
     res
       .status(500)
-      .json({ success: false, message: 'Error checking menu name' })
+      .json({ success: false, message: "Error checking menu name" });
   }
-})
+});
 
-router.put('/:id', parser.single('image'), async (req, res) => {
-  const { id } = req.params
-  const { name, description, price, sweetLevel, type, recipe } = req.body
+router.put("/:id", parser.single("image"), async (req, res) => {
+  const { id } = req.params;
+  const { name, description, price, sweetLevel, type, recipe } = req.body;
 
   try {
     let updatedData = {
@@ -116,57 +116,57 @@ router.put('/:id', parser.single('image'), async (req, res) => {
       sweetLevel, // Ensure this is included
       type, // Ensure this is included
       recipe,
-    }
+    };
     if (req.file) {
-      const imageUrl = req.file.path
+      const imageUrl = req.file.path;
       const result = await cloudinary.uploader.upload(imageUrl, {
-        folder: 'menus',
-      })
-      updatedData.image = result.secure_url
+        folder: "menus",
+      });
+      updatedData.image = result.secure_url;
     }
 
     const updatedItem = await Menu.findByIdAndUpdate(id, updatedData, {
       new: true,
-    })
+    });
 
     if (!updatedItem) {
       return res
         .status(404)
-        .json({ success: false, message: 'Menu item not found' })
+        .json({ success: false, message: "Menu item not found" });
     }
 
     res.json({
       success: true,
-      message: 'Menu item updated successfully',
+      message: "Menu item updated successfully",
       menuItem: updatedItem,
-    })
+    });
   } catch (error) {
-    console.error(error)
+    console.error(error);
     res
       .status(500)
-      .json({ success: false, message: 'Error updating menu item' })
+      .json({ success: false, message: "Error updating menu item" });
   }
-})
+});
 
-router.get('/menu/:id', async (req, res) => {
-  const { id } = req.params // Extract the ID from the request parameters
+router.get("/menu/:id", async (req, res) => {
+  const { id } = req.params; // Extract the ID from the request parameters
 
   try {
-    const menuItem = await Menu.findById(id).populate('recipe') // Assuming 'recipe' is a reference in your Menu model that you want populated
+    const menuItem = await Menu.findById(id).populate("recipe"); // Assuming 'recipe' is a reference in your Menu model that you want populated
 
     if (!menuItem) {
       return res
         .status(404)
-        .json({ success: false, message: 'Menu item not found' })
+        .json({ success: false, message: "Menu item not found" });
     }
 
-    res.status(200).json(menuItem)
+    res.status(200).json(menuItem);
   } catch (error) {
-    console.error(error)
+    console.error(error);
     res
       .status(500)
-      .json({ success: false, message: 'Error fetching menu item details' })
+      .json({ success: false, message: "Error fetching menu item details" });
   }
-})
+});
 
-module.exports = router
+module.exports = router;
